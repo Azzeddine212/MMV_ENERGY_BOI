1cd799cdb6382275f23cd678209a03bc0841fc06
Azzeddine RAH <158000540+Azzeddine212@users.noreply.github.com>
2025-03-02 13:36:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,7 +104,7 @@ uploaded_file = st.file_uploader("📂 Téléchargez votre fichier Excel", type=
 if uploaded_file is not None:
     data_boiry = pd.read_excel(uploaded_file, index_col='Date')
     st.success("✅ Fichier chargé avec succès !")
-    st.dataframe(dvariables.describe())
+    st.dataframe(variables.describe())
     
     model_path = "xgb_model_cb22-23-24_10_param.joblib"
     scaler_path = "scaler_cb22-23-24_10_param.pkl"
