5d3e0560f13b05cd3523efb5e61963920c6261c9
Azzeddine RAH <158000540+Azzeddine212@users.noreply.github.com>
2025-03-02 19:34:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -192,15 +192,6 @@ if uploaded_file is not None:
                 ax.grid(True)
                 st.pyplot(fig)
 
-                # Ajout de sélecteurs et boutons dans le panneau latéral
-                option = st.sidebar.selectbox("Sélectionnez une option :", ["Option 1", "Option 2", "Option 3"])
-                if st.sidebar.button("Valider"):
-                    st.sidebar.success(f"✅ Vous avez choisi : {option}")
-
-
-                option = st.sidebar.selectbox("Sélectionnez une option :", ["Option 1", "Option 2", "Option 3"])
-                if st.sidebar.button("Valider"):
-                        st.sidebar.success(f"✅ Vous avez choisi : {option}")
                 # Onglets
                 #tab1, tab2, tab3 = st.tabs(["📊 Prédictions(Métriques)", "📈 statistiques & Analyse", "📥 Télécharger"])
 
