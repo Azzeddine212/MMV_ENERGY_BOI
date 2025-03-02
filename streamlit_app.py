bf1cc1a219641f12336bbef03e4f294148029f20
Azzeddine RAH <158000540+Azzeddine212@users.noreply.github.com>
2025-03-02 16:17:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,8 +166,8 @@ if uploaded_file is not None:
                 fig, ax = plt.subplots(figsize=(20, 10))
                 mean = df_results["Prédictions"].mean()
                 std_dev = df_results["Prédictions"].std()
-                upper_limit = mean + 3 * std_dev
-                lower_limit = mean - 3 * std_dev
+                upper_limit = mean + 2 * std_dev
+                lower_limit = mean - 2 * std_dev
 
                 # Ajouter une ligne horizontale représentant l'objectif
                 ax.axhline(y=objectif, color="green", linestyle="--", linewidth=2, label=f'Objectif : {objectif} kWh')
@@ -176,9 +176,9 @@ if uploaded_file is not None:
                 au_dessus = df_results["Prédictions"] > objectif  # Masque booléen
                 ax.scatter(df_results.index[au_dessus], df_results["Prédictions"][au_dessus], color="green", label="Au-dessus de l'objectif", zorder=3)
     
-                ax.axhline(upper_limit, color="red", linestyle="dashed", linewidth=1, label=f"Mean + 3σ = {upper_limit:.2f}")
-                ax.axhline(lower_limit, color="red", linestyle="dashed", linewidth=1, label=f"Mean - 3σ = {lower_limit:.2f}")
-                ax.plot(df_results.index, df_results["Prédictions"], color="blue", label='Prédiction CB24', alpha=0.6)
+                ax.axhline(upper_limit, color="red", linestyle="dashed", linewidth=1, label=f"Mean + 2σ = {upper_limit:.2f}")
+                ax.axhline(lower_limit, color="red", linestyle="dashed", linewidth=1, label=f"Mean - 2σ = {lower_limit:.2f}")
+                ax.plot(df_results.index, df_results["Prédictions"], color="black", label='Prédiction CB24', alpha=0.6)
                 #ax.bar(df_results.index, df_results["Prédictions"], color="red", label='Prédiction CB24', alpha=0.6)
                 ax.set_title("Prédiction CB24")
                 ax.set_xlabel("Date")
