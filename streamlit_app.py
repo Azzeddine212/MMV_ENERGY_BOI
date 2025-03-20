3fc9a505c9fa3e4b3ab542bac6ffe4a49a670bd7
Azzeddine RAH <158000540+Azzeddine212@users.noreply.github.com>
2025-03-20 17:46:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -242,7 +242,8 @@ if uploaded_file is not None:
                     width = patch.get_width()
                     x_position = patch.get_x() + width / 2
                     percentage = (height / total) * 100
-                    ax.text(x_position, height + 0.1, f'{percentage:.1f}%', ha='center', fontsize=14)
+                    if percentage > 0 :
+                        ax.text(x_position, height + 0.1, f'{percentage:.1f}%', ha='center', fontsize=14)
                 
                 # Ajouter des titres et labels
                 ax.set_title("Histogramme des Prédictions de Consommation Énergétique", fontsize=14)
